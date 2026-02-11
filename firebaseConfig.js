614ea1f3180289e14e91d8de414a268eaea64562
porcyn <chayanid.ca@mail.wu.ac.th>
2026-02-11 23:46:09 +0700

    update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp, getApps, getApp } from "firebase/app"; // เพิ่�
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
